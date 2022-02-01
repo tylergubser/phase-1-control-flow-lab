7582d9d4aca3c66dbf5ad7a196f55e38eb6aa43d
tylergubser <tylergubser2200@gmail.com>
2022-01-31 18:58:48 -0600

    commit tree 1c102f7255ccb6e84ee4327d8cdcb15f56009727

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!';
+  }
+  else if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (ride > 2500) {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? (city = "Ok, sounds good.") : (city = "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous': tip = 'Thank you so much.';
+    break;
+    case 'not as generous': tip = 'Thank you.';
+    break;
+    default: tip = 'Bye.';
+    break;
+  }
+  return tip;
 }
